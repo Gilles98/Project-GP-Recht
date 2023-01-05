52379969c16e7d1907a29738a5b39a6513f2711e
Gilles98 <60871307+Gilles98@users.noreply.github.com>
2023-01-05 16:57:23 +0100

    Update Dialog.cs

--- Project_Recht/Service/Dialog.cs
+++ Project_Recht/Service/Dialog.cs
@@ -7,14 +7,17 @@ using System.Windows;
 
 namespace Project_Recht.Service
 {
+
+//this class works as a indirect between the viewmodel and the view to get the result, and show, messageboxes (small popups) 
     public class Dialog: IDialog
     {
-        //to call messageboxes in my viewmodels but not break 
+        //to call messageboxes in my viewmodels but not break the mvvm pattern
         public void ToonMessageBox(string bericht)
         {
             MessageBox.Show(bericht);
         }
 
+        // to get the result if based on what is clicked in the popup messagebox
         public bool ToonMessageBoxPlusReturnAntwoord(string bericht, string titel)
         {
             bool resultaat = false;
